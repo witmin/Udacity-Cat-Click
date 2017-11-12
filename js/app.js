3c53baa9b20ed53838af062371a10e98067e9dab
Millie Lin <linxinmin@gmail.com>
2017-11-12 16:41:23 +0800

    Add cat constructor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,39 @@
-var clickTime = 0;
+/**
+ * @description define the click total
+ * @type {number}
+ */
+let clicks = 0;
 
+const imageFolder = "images/";
+
+/**
+ * @description set up a Class of cat
+ * @constructor
+ * @param name {string}, image {string}
+ */
+
+class Cat {
+    constructor(name, image) {
+        this._name = name;
+        this._image = image;
+    }
+
+    addToPage() {
+        let htmlContainer = `<li><h2>${this._name}</h2>
+            <figure><img src="${this._image}" alt="${this._name}"></figure>
+            </li>`;
+
+        $('#cats-list').append($(htmlContainer));
+    };
+
+    imageClicked() {
+        clicks += 1;
+    };
+}
+
+/**
+ * @description init image click times value
+ */
 function initClickTimesValue() {
     $('#clicked-times').text(clickTime);
 }
@@ -8,8 +42,12 @@ function initClickTimesValue() {
 $(document).ready(function () {
     initClickTimesValue();
 
-    $('#kitten-image').click(function (e) {
-        clickTime += 1;
+
+
+    let cat_one = new Cat("Birdy", "")
+
+    $('.cat-image').click(function (e) {
+        clicks += 1;
         $('#clicked-times').text(clickTime);
     });
 });
