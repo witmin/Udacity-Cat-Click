a3336f946430507f50a6185f7bf9516cdc366351
Millie Lin <linxinmin@gmail.com>
2017-11-12 21:15:09 +0800

    Refactor the code with MOC method.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,34 @@
-/**
- * @description set up a Class of cat
- * @constructor
- * @param name {string}, image {string}
- */
-class Cat {
-    constructor(name, image) {
-        this._name = name;
-        this._image = image;
-    }
-
-    addToPage() {
-        let htmlContainer = `<li><h2>${this._name}</h2><figure><img class="cat-image" src="${this._image}" alt="${this._name}"></figure></li>`;
-
-        $('#cats-list').empty();
-
-        $('#cats-list').append($(htmlContainer));
-    };
-
-    addToList() {
-        let htmlContainer = `<li><a href="#" class="cat-name"  title="${this._name}">${this._name}</a></li>`;
-
-        $('#cats-names-list').append($(htmlContainer));
-    };
-}
-
-
 $(function () {
     /**
-     * @description define the click total
-     * @type {number}
+     * @description set up a Class of cat
+     * @constructor
+     * @param name {string}, image {string}
      */
-    let clicks = 0;
-    let selectedCatName = '';
-    const imageFolder = "images/";
+    class Cat {
+        constructor(name, image) {
+            this._name = name;
+            this._image = image;
+        }
 
+        addToPage() {
+            let htmlContainer = `<li><h2>${this._name}</h2><figure><img class="cat-image" src="${this._image}" alt="${this._name}"></figure></li>`;
+
+            $('#cats-list').empty();
+
+            $('#cats-list').append($(htmlContainer));
+        };
+
+        addToList() {
+            let htmlContainer = `<li><a href="#" class="cat-name"  title="${this._name}">${this._name}</a></li>`;
+
+            $('#cats-names-list').append($(htmlContainer));
+        };
+    }
 
     /**
      * Create new cat object and add them to an array
      */
-
+    let imageFolder = "images/";
     let cat1 = new Cat("Birdy", imageFolder + "kitten-2.jpg");
     let cat2 = new Cat("Cindy", imageFolder + "kitten-7.jpg");
     let cat3 = new Cat("Ninja", imageFolder + "kitten-Nirzar.jpg");
@@ -46,55 +36,61 @@ $(function () {
     let cat5 = new Cat("Twins", imageFolder + "kitten-twins.jpg");
     let cats = [cat1, cat2, cat3, cat4, cat5];
 
-    /**
-     * @description init image click times value
-     */
-    function initClickTimesValue() {
-        $('#clicked-times').text(clicks);
-    }
+    let modal = {
+        clicks: 0,
+        selectedCatName: '',
+    };
 
-    /**
-     * @description load cats name into a list
-     */
-    function loadCatsList(cats) {
-        for (let cat of cats) {
-            cat.addToList();
-        }
-    }
 
-    /**
-     * @description show selected cat image
-     */
+    let view = {
+        initClickTimesValue: function (clicks) {
+            $('#clicked-times').text(clicks);
+        },
 
-    function showSelectedCat(selectedCatName) {
-        for (let cat of cats) {
-            if (cat._name === selectedCatName) {
-                console.log(cat._name === selectedCatName);
-                cat.addToPage();
+        loadCatsList: function (cats) {
+            for (let cat of cats) {
+                cat.addToList();
+            }
+        },
+        /**
+         * @description show selected cat image
+         */
+        showSelectedCat: function (selectedCatName) {
+            for (let cat of cats) {
+                if (cat._name === selectedCatName) {
+                    console.log(cat._name === selectedCatName);
+                    cat.addToPage();
+                }
             }
         }
-    }
+    };
 
     /**
      * @description initial the click value and load cats list
      */
 
-    initClickTimesValue();
-    loadCatsList(cats);
+    let octopus = {
+        render: function () {
+            view.initClickTimesValue(modal.clicks);
+            view.loadCatsList(cats);
 
-    /**
-     * Listen to click events
-     */
-    $('#cats-names-list').click('li', function (e) {
-        let target = event.target;
-        selectedCatName = $(target).attr('title');
-        showSelectedCat(selectedCatName);
-    });
+            /**
+             * Listen to click events
+             */
+            $('#cats-names-list').click('li', function (e) {
+                let target = event.target;
+                modal.selectedCatName = $(target).attr('title');
+                view.showSelectedCat(modal.selectedCatName);
+            });
 
-    $('#cats-list').click('img', function (e) {
-        console.log(event.target);
-        clicks += 1;
-        $('#clicked-times').text(clicks);
-    });
+            $('#cats-list').click('img', function (e) {
+                console.log(event.target);
+                modal.clicks += 1;
+                $('#clicked-times').text(modal.clicks);
+            });
+        }
+    };
+
+    octopus.render();
 
 });
