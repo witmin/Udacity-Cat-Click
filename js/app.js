5692102b381612f08afa880036df7c3f9325abc9
Millie Lin <linxinmin@gmail.com>
2017-11-12 20:51:45 +0800

    separate class and functions.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,8 @@
 /**
- * @description define the click total
- * @type {number}
- */
-let clicks = 0;
-let selectedCatName = '';
-const imageFolder = "images/";
-
-
-/**
- * Create new cat object and add them to an array
- */
-
-let cat1 = new Cat("Birdy", imageFolder + "kitten-2.jpg");
-let cat2 = new Cat("Cindy", imageFolder + "kitten-7.jpg");
-let cat3 = new Cat("Ninja", imageFolder + "kitten-Nirzar.jpg");
-let cat4 = new Cat("Thai", imageFolder + "kitten-Thai.jpg");
-let cat5 = new Cat("Twins", imageFolder + "kitten-twins.jpg");
-let cats = [cat1, cat2, cat3, cat4, cat5];
-
-/**
  * @description set up a Class of cat
  * @constructor
  * @param name {string}, image {string}
  */
-
 class Cat {
     constructor(name, image) {
         this._name = name;
@@ -45,55 +24,73 @@ class Cat {
     };
 }
 
-/**
- * @description init image click times value
- */
-function initClickTimesValue() {
-    $('#clicked-times').text(clicks);
-}
-
-/**
- * @description load all cats in cats array
- */
-function loadCats(cats) {
-    for (let cat of cats) {
-        cat.addToPage();
-    }
-}
-
-/**
- * @description load cats name into a list
- */
-function loadCatsList(cats) {
-    for (let cat of cats) {
-        cat.addToList();
-    }
-}
-
-/**
- * @description show selected cat image
- */
-
-function showSelectedCat(selectedCatName) {
-    for (let cat of cats) {
-        if (cat._name === selectedCatName ){
-            console.log(cat._name === selectedCatName);
-            cat.addToPage();
-        }
-    }
-}
 
 $(function () {
+    /**
+     * @description define the click total
+     * @type {number}
+     */
+    let clicks = 0;
+    let selectedCatName = '';
+    const imageFolder = "images/";
+
+
+    /**
+     * Create new cat object and add them to an array
+     */
+
+    let cat1 = new Cat("Birdy", imageFolder + "kitten-2.jpg");
+    let cat2 = new Cat("Cindy", imageFolder + "kitten-7.jpg");
+    let cat3 = new Cat("Ninja", imageFolder + "kitten-Nirzar.jpg");
+    let cat4 = new Cat("Thai", imageFolder + "kitten-Thai.jpg");
+    let cat5 = new Cat("Twins", imageFolder + "kitten-twins.jpg");
+    let cats = [cat1, cat2, cat3, cat4, cat5];
+
+    /**
+     * @description init image click times value
+     */
+    function initClickTimesValue() {
+        $('#clicked-times').text(clicks);
+    }
+
+    /**
+     * @description load cats name into a list
+     */
+    function loadCatsList(cats) {
+        for (let cat of cats) {
+            cat.addToList();
+        }
+    }
+
+    /**
+     * @description show selected cat image
+     */
+
+    function showSelectedCat(selectedCatName) {
+        for (let cat of cats) {
+            if (cat._name === selectedCatName) {
+                console.log(cat._name === selectedCatName);
+                cat.addToPage();
+            }
+        }
+    }
+
+    /**
+     * @description initial the click value and load cats list
+     */
+
     initClickTimesValue();
     loadCatsList(cats);
 
+    /**
+     * Listen to click events
+     */
     $('#cats-names-list').click('li', function (e) {
         let target = event.target;
         selectedCatName = $(target).attr('title');
         showSelectedCat(selectedCatName);
     });
 
-     // Listen to click event
     $('#cats-list').click('img', function (e) {
         console.log(event.target);
         clicks += 1;
